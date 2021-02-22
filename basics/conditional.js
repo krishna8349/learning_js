14552b607a24b84327d9cd20bd8d4a250388ab36
Krishna Sisodiya <ksisodiya196@gmail.com>
2021-02-22 15:44:45 +0530

    added a program for condition

---
+++ basics/conditional.js
@@ -0,0 +1,17 @@
+var i=100;
+var j=230;
+var k=34;
+
+if(i>j){
+    console.log("i is large")
+}else{
+    console.log("j is large")
+}
+
+if(i>j && i>k){
+    console.log("i is lagre")
+}else if(j>k && j>i){
+    console.log("j is large")
+}else{
+    console.log("k is large")
+}
