9645a1fbbb4f79186eb6535cf915324e81122296
Krishna Sisodiya <ksisodiya196@gmail.com>
2021-02-23 16:49:27 +0530

    Added a program for number

---
+++ w3s/number.js
@@ -0,0 +1,17 @@
+var n =10.34;
+
+//number method 
+console.log(n.toString());
+console.log(n.toExponential());
+console.log(n.toFixed(3));
+
+// Global method
+console.log(Number(n));
+console.log(parseFloat(n));
+console.log(parseInt(n));
+
+// Number properties
+console.log(Number.MAX_VALUE);
+console.log(Number.MIN_VALUE);
+console.log(Number.POSITIVE_INFINITY);
+console.log(Number.NEGATIVE_INFINITY);
