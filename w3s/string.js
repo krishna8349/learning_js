4ea9ca018bdef2c5f8758a824f4f2b25c24f1e14
Krishna Sisodiya <ksisodiya196@gmail.com>
2021-02-23 15:38:52 +0530

    Added a program for string

---
+++ w3s/string.js
@@ -0,0 +1,17 @@
+const name="coding";
+const name2= 'programming';
+const nmae3= 'vscodes';
+
+console.log(name);
+console.log(name2);
+console.log(nmae3);
+
+// find a word in a sentence
+var sent = "my name is krishna is";
+var word_to_find = "is";
+
+// word exists or not
+console.log(sent.includes(word_to_find))
+
+// position of word
+console.log(sent.indexOf(word_to_find))
